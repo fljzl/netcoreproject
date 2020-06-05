855fda40edfc531c6a0a9b6b89e9c79e17b12ea1
bert <bert@DESKTOP-5875RFQ>
2020-06-05 14:06:00 +0800

    msg

---
+++ netcoreproject/apple.web.cms.quartz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace apple.web.cms.quartz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
